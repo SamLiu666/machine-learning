5c60e1934cc757d3c196ba25ec664aa7f869ef02
SamLiu666 <aa2083683710@gmail.com>
2020-08-07 11:49:16 +0800

    cv

--- DM_class/CV/assignment 2/pil/circle.py
+++ DM_class/CV/assignment 2/pil/circle.py
@@ -8,27 +8,24 @@ from skimage import color, io,feature
 import numpy as np
 
 
-# Load image:
-# input_image = Image.open("input.jpg")
-# input_image_rgb = color.rgb2gray(input_image__1)  # rgb2gray
-input_image = io.imread("input.jpg")
+input_image = io.imread("input.jpg")   # Load image
 input_image_gray = color.rgb2gray(input_image)   # rgb2gray
-input_image_canny = feature.canny(input_image_gray, sigma=1)
+input_image_canny = feature.canny(input_image_gray, sigma=1)   # edge detector, also can use the assignment1
+print(input_image_canny.shape)
+output = denoise_bilateral(input_image_canny, sigma_color=0.01, sigma_spatial=5, multichannel=True)
+
 plt.imshow(input_image_canny)
 plt.title("canny")
 plt.show()
 
-# Output image:
-# output_image = Image.new("RGB", input_image.size)
-# output_image.paste(input_image)
-# draw_result = ImageDraw.Draw(output_image)
-
-output_image = np.zeros_like(input_image)
+#output_image = np.zeros_like(input_image)
+output_image = np.zeros_like(input_image_gray)
 print(output_image.shape)
-# Find circles
+
+# define circles
 rmin = 18
-# rmax = 20
-rmax = 100
+rmax = 20
+# rmax = 100
 steps = 100
 threshold = 0.4
 
@@ -36,15 +33,6 @@ points = []
 for r in range(rmin, rmax + 1):
     for t in range(steps):
         points.append((r, int(r * cos(2 * pi * t / steps)), int(r * sin(2 * pi * t / steps))))
-
-# my canny edge detector from assignment 1
-# img_gaussian = gaussian(k=5, sigma=1)  # 1. Gaussian  with kenel=5x5
-# img_smoothed = convolve(input_image_1, img_gaussian)  # Gaussian smooth
-# gradient, theta = sobel_filters(img_smoothed)  # 2. Compute magnitude and orientation of gradient
-# suppression_img = comput_Non_maximum_suppression(gradient,theta)  # 3. Compute Non-maximum suppression:
-# threshold_img, low_threshold, high_threshold = threshold(suppression_img)  # 4.1 Define two thresholds: low and high
-# final_img = myCannyEdgeDetector(threshold_img, Low_Threshold=low_threshold, High_Threshold=high_threshold)
-
 
 acc = defaultdict(int)
 for x, y in canny_edge_detector(input_image):
