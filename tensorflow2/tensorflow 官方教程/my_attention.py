32961f88a84fa2eeb9b9b19a34a0b223b3b9f7d2
SamLiu666 <aa2083683710@gmail.com>
2020-10-04 20:01:44 +0800

    tensorflow2

---
+++ tensorflow2/tensorflow 官方教程/my_attention.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+class Encoder(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+    super(Encoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.enc_units = enc_units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+  def call(self, x, hidden):
+    x = self.embedding(x)
+    output, state = self.gru(x, initial_state = hidden)
+    return output, state
+
+  def initialize_hidden_state(self):
+    return tf.zeros((self.batch_sz, self.enc_units))
+
+if __name__ == '__main__':
+    encoder = Encoder(vocab_size=6453, embedding_dim=256
+                      , enc_units=1024, batch_sz=64)
+    # 样本输入
+    sample_hidden = encoder.initialize_hidden_state()
+    sample_output, sample_hidden = encoder(tf.random.normal([64,38]), sample_hidden)
+    print('Encoder output shape: (batch size, sequence length, units) {}'.format(sample_output.shape))
+    print('Encoder Hidden state shape: (batch size, units) {}'.format(sample_hidden.shape))
